aedf8102c89ff60ec43b2265c6427dfca61d943d
geavenx <vitordonnangeloc@gmail.com>
2023-09-04 11:55:10 -0300

    removed comment

--- src/app/dashboard/phones/search/page.js
+++ src/app/dashboard/phones/search/page.js
@@ -111,17 +111,6 @@ export default function Search() {
                     <div className="p-4 md:w-1/3 flex flex-col text-center items-center"></div>
                     <div className="p-4 md:w-1/3 flex flex-col text-center items-center">
                       <div className="w-20 h-20 inline-flex items-center justify-center rounded-full bg-purple-100 text-purple-500 mb-5 flex-shrink-0">
-                      {/* <svg
-                        fill="none"
-                        stroke="currentColor"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        className="w-10 h-10"
-                        viewBox="0 0 24 24"
-                      >
-                        <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12" />
-                      </svg> */}
                       <svg 
                         className="w-12 h-12" 
                         viewBox="0 0 20 20"
