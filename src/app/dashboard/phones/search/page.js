508674da75e81de78e6714c7a2e42138b127dd0b
geavenx <vitordonnangeloc@gmail.com>
2023-09-04 11:52:15 -0300

    fixed SVG icon

--- src/app/dashboard/phones/search/page.js
+++ src/app/dashboard/phones/search/page.js
@@ -4,6 +4,7 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import Loading from '@/app/components/loading';
 import { useSearchParams } from 'next/navigation';
+import Link from 'next/link';
 
 
 export default function Search() {
@@ -110,32 +111,33 @@ export default function Search() {
                     <div className="p-4 md:w-1/3 flex flex-col text-center items-center"></div>
                     <div className="p-4 md:w-1/3 flex flex-col text-center items-center">
                       <div className="w-20 h-20 inline-flex items-center justify-center rounded-full bg-purple-100 text-purple-500 mb-5 flex-shrink-0">
-                        <svg
-                          fill="none"
-                          stroke="currentColor"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          className="w-10 h-10"
-                          viewBox="0 0 24 24"
-                        >
-                          <circle
-                            cx="6"
-                            cy="6"
-                            r="3"
-                          />
-                          <circle
-                            cx="6"
-                            cy="18"
-                            r="3"
-                          />
-                          <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12" />
-                        </svg>
+                      {/* <svg
+                        fill="none"
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        className="w-10 h-10"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12" />
+                      </svg> */}
+                      <svg 
+                        className="w-12 h-12" 
+                        viewBox="0 0 20 20"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="1"
+                      >
+							          <path d="M10.185,1.417c-4.741,0-8.583,3.842-8.583,8.583c0,4.74,3.842,8.582,8.583,8.582S18.768,14.74,18.768,10C18.768,5.259,14.926,1.417,10.185,1.417 M10.185,17.68c-4.235,0-7.679-3.445-7.679-7.68c0-4.235,3.444-7.679,7.679-7.679S17.864,5.765,17.864,10C17.864,14.234,14.42,17.68,10.185,17.68 M10.824,10l2.842-2.844c0.178-0.176,0.178-0.46,0-0.637c-0.177-0.178-0.461-0.178-0.637,0l-2.844,2.841L7.341,6.52c-0.176-0.178-0.46-0.178-0.637,0c-0.178,0.176-0.178,0.461,0,0.637L9.546,10l-2.841,2.844c-0.178,0.176-0.178,0.461,0,0.637c0.178,0.178,0.459,0.178,0.637,0l2.844-2.841l2.844,2.841c0.178,0.178,0.459,0.178,0.637,0c0.178-0.176,0.178-0.461,0-0.637L10.824,10z"></path>
+						          </svg>
                       </div>
                     </div>
                   </div>
                   <button className="flex mx-auto mt-8 text-white bg-purple-500 border-0 py-2 px-8 focus:outline-none hover:bg-purple-600 rounded text-lg">
-                    <a href="/dashboard">Voltar</a>
+                    <Link href="/dashboard">Voltar</Link>
                   </button>
                 </div>
               </section>
