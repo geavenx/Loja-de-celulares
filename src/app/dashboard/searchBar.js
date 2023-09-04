c4b62a6b1423fe59f21fee916aa33a9c68e3e769
geavenx <vitordonnangeloc@gmail.com>
2023-09-04 11:54:51 -0300

    added required attribute to input

--- src/app/dashboard/searchBar.js
+++ src/app/dashboard/searchBar.js
@@ -28,6 +28,7 @@ export default function SearchBar() {
         type="text"
         id="marca"
         name="marca"
+        required
         className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-purple-500 focus:bg-transparent focus:ring-2 focus:ring-purple-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
       />
       <button
